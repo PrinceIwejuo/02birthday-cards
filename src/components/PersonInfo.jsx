bcf1bea816fa965bd6624ee614322455c0250c22
PrinceIwejuo <Princeiwejuo@gmail.com>
2022-03-09 19:26:30 -0500

    PuttinItTogether

--- src/components/PersonInfo.jsx
+++ src/components/PersonInfo.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 
 class PersonInfo extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            Age: this.props.Age
+        }
+    };
+
+    inceaseAge = () => {
+        this.setState({Age: parseInt(this.state.Age) + 1})
+    }
+
     render(){
         return (
             <div>
                 <h3>{this.props.personName}</h3>
-                <p>{this.props.Age}</p>
-                <p>{this.props.haircolor} </p>
+                <p>Age: {this.state.Age}</p>
+                <p>Hair Color: {this.props.haircolor} </p>
+                <button onClick = {this.inceaseAge}>Birthday Button For {this.props.personName}</button>
             </div>
         )
     }
